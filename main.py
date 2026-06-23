22e533b98d840c2c3e42dda74d5169a4d2c4702e
Faizal <nightowlclub72@gmail.com>
2026-06-23 15:16:26 +0000

    fixed screen

--- main.py
+++ main.py
@@ -34,12 +34,62 @@ def angelina():
 def begger():
     st.title("Case 1")
     st.header("Murder of 'Normal' begger")
+
     st.subheader(" A old bgeer known by Mr. Grey found dead beneath the railway bridge..")
 
-    if st.button("Examin the body"):
-        pass
+    menu()
+
     
-    elif st.button("Investigate Crime scene")
+
+def body():
+        st.subheader("Eveyone beleived that he was just a poor begger but in investigation we have found that the man posses a gold poclet watch engraved with sign 'A.W' .. and. a Key to a safe ....")
+        st.subheader("Investigation reveals that Mr. grey was a before Arthur A wealthy businessman who dissappears 20 years ago.")
+
+        if st.button("Return to menu"):
+            menu()
+
+def c_scene():
+        st.subheader("Blood is spilled everywhere beneath the bridge and there is a knife without any fingerprints... ")
+
+        if st.button("Return to menu "):
+            menu()
+
+def suspect():
+        if st.button("1. Jack Miller (Former partner)"):
+            jack()
+
+        if st.button("2. Sarah (estrangled daughter)"):
+            pass
+        if st.button("3. Ethan brooks (investigate journalist)"):
+            pass
+
+        if st.button("4. Fathee thomas (local priest )"):
+            pass
+
+def jack():
+        st.subheader("Jack Miller")
+
+        st.markdown("Age : 68 ")
+        st.markdown("Former business partner")
+
+        st.markdown("Current CEO of whitmore indutries.")
+        st.markdown("Clue : Arthru return would destroy everything jack built ")
+
+        st.markdown("Secret : He forged documents to steal company")
+
+def menu():
+        if st.button("Examin the body"):
+           
+           st.session_state.stage = "body"
+    
+        if st.button("Investigate Crime scene"):
+           st.session_state.stage = "c_scene"
+
+        if st.button("Questions the suspects."):
+           st.session_state.stage = "suspect"
+
+    
+
 
 
     
@@ -58,6 +108,9 @@ elif st.session_state.stage == "case2":
 
 elif st.session_state.stage == "case3":
     angelina()
+
+elif st.session_state.stage == "suspect":
+     suspect()
 
 
 
